ef7778c36733f90a2ee7034086cda87c5e079616
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-05-10 22:03:44 +0200

    add and export db.js functions to let app connect to local mongoDB database

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const { connectToDb, getDb } = require("./db");
 
 // init app & middleware
 const app = express();
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
+// db connection
+let db;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("app listening on port 3000");
+    });
+    db = getDb();
+  }
 });
 
 app.get("/", (req, res) => {
-  const request = req.method
-  const path = req.path
-res.send(`<p>request method: ${request}</p> <p>request path: ${path}</p>`);
+  const request = req.method;
+  const path = req.path;
+  res.send(`<p>request method: ${request}</p> <p>request path: ${path}</p>`);
 });
 
-app.get('/books', (req, res) => {
-    res.json({mssg: "welcome to the api"})
-})
+app.get("/books", (req, res) => {
+  res.json({ mssg: "welcome to the api" });
+});
