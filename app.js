15f924f0312c81ae44ee45cdf78a8ff4fb1fc6e2
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-05-11 07:54:45 +0200

    chore: Add routes to fetch books and book by ID

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
 
 // init app & middleware
 const app = express();
@@ -23,5 +24,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/books", (req, res) => {
-  res.json({ mssg: "welcome to the api" });
+  let books = [];
+
+  db.collection("books")
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Could not fetch the documents" });
+    });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({error: 'Not a valid doc id'})
+  }
 });
