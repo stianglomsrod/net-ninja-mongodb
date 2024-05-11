295d37de1e8708c1b8c42a4a5e87be037eb5eb3b
stianglomsrod <112327355+stianglomsrod@users.noreply.github.com>
2024-05-11 08:35:12 +0200

    chore: Add routes to fetch books, create a new book, update a book, and delete a book. Add pagination using .skip() and .limit() .

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { ObjectId } = require("mongodb");
 
 // init app & middleware
 const app = express();
+app.use(express.json());
 
 // db connection
 let db;
@@ -24,11 +25,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/books", (req, res) => {
+  // current page
+  const page = req.query.p || 0;
+  const booksPerPage = 3;
+
   let books = [];
 
   db.collection("books")
     .find()
     .sort({ author: 1 })
+    .skip(page * booksPerPage)
+    .limit(booksPerPage)
     .forEach((book) => books.push(book))
     .then(() => {
       res.status(200).json(books);
@@ -49,6 +56,51 @@ app.get("/books/:id", (req, res) => {
         res.status(500).json({ error: "Could not fetch the document" });
       });
   } else {
-    res.status(500).json({error: 'Not a valid doc id'})
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Could not create a new document" });
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: new ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not delete the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
+
+app.patch("/books/:id", (req, res) => {
+  const updates = req.body;
+
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not update the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
   }
 });
